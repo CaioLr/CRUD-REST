e0ae981911157c5accbc10000cc35131113d8caf
CaioLr <56622666+CaioLr@users.noreply.github.com>
2021-12-10 17:56:43 -0300

    finalizando

--- app/utils/create.sql
+++ app/utils/create.sql
@@ -5,6 +5,6 @@ use web3;
 CREATE TABLE usuarios(
     id_usuario int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome varchar(50) NOT NULL,
-    email varchar(50) NOT NULL+
+    email varchar(50) NOT NULL
 );
 
